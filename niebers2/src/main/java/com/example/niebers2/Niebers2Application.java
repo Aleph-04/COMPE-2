35c7ff9e90a81df4c538885663761b939501c8da
Aleph-04 <139783895+Aleph-04@users.noreply.github.com>
2025-05-18 13:52:40 +0800

    maven

---
+++ niebers2/src/main/java/com/example/niebers2/Niebers2Application.java
@@ -0,0 +1,13 @@
+package com.example.niebers2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Niebers2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Niebers2Application.class, args);
+	}
+
+}
---
+++ niebers2/src/test/java/com/example/niebers2/Niebers2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.niebers2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Niebers2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
