6b9cc107493a8f83498d7c659214f284e2d1c4b4
Aleph-04 <139783895+Aleph-04@users.noreply.github.com>
2025-05-18 10:57:02 +0800

    initial
    inital commit

---
+++ niebers/niebers/src/main/java/com/example/niebers/NiebersApplication.java
@@ -0,0 +1,13 @@
+package com.example.niebers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiebersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiebersApplication.class, args);
+	}
+
+}
---
+++ niebers/niebers/src/test/java/com/example/niebers/NiebersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.niebers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiebersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
